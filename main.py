b87e85dbd8e17159cd6daae0effa1359d247e0a7
classiccls <classiccls@gmail.com>
2025-03-22 22:56:22 +0100

    05.dodanie sprita kola

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    base_clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+        screen.fill(("blue"))
         pygame.display.flip()
+        dt = base_clock.tick(60) / 1000
+
     
     #print("Starting Asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
