3758657f64b2af90040a8461f6a733dde69f80bc
classiccls <classiccls@gmail.com>
2025-03-24 00:03:13 +0100

    06.dodanie playera

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import * 
+from player import Player
 
 def main():
     pygame.init()
@@ -8,11 +9,23 @@ def main():
     base_clock = pygame.time.Clock()
     dt = 0
 
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    Player.containers = (updatables, drawables)
+
+    Player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(("blue"))
+        screen.fill(("black"))
+        
+        updatables.update(dt)
+        for drawable in drawables:
+            drawable.draw(screen)
+
         pygame.display.flip()
         dt = base_clock.tick(60) / 1000
 
